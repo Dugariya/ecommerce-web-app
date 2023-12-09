0f3c428c845f388f4148a4c4f704d3525c5f0097
Dugariya <pawandugariya@gmail.com>
2023-12-09 14:52:48 +0530

    update nav bar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { FaBars, FaTimes, FaShoppingCart, FaUser } from "react-icons/fa"; // Assuming you are using React Icons for the menu, cart, and profile icons
 
 interface Route {
   path: string;
@@ -11,32 +12,50 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ routes }) => {
   const location = useLocation();
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
   return (
-    <nav className="bg-gray-800 p-4">
-      <ul className="flex flex-col md:flex-row md:items-center">
-        {routes.map((route, index) => (
-          <li
-            key={index}
-            className={`my-2 md:mx-6 md:my-0 ${
-              location.pathname === route.path ? "bg-gray-600" : ""
-            }`}
-          >
-            <Link to={route.path} className="text-white hover:text-gray-300">
-              {route.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <nav className="bg-gray-800 p-4 flex items-center justify-between">
+      <div className="flex items-center space-x-4">
+        <button
+          className="text-white focus:outline-none md:hidden"
+          onClick={toggleDrawer}
+        >
+          {drawerOpen ? <FaTimes /> : <FaBars />}
+        </button>
+        <Link to="/" className="text-white hover:text-gray-300">
+          Home
+        </Link>
+      </div>
+      <div className="hidden md:flex items-center space-x-4">
+        <Link to="/cart" className="text-white hover:text-gray-300">
+          <FaShoppingCart />
+        </Link>
+        <Link to="/profile" className="text-white hover:text-gray-300">
+          <FaUser />
+        </Link>
+      </div>
+      {drawerOpen && (
+        <div className="md:hidden fixed inset-0 bg-gray-800 bg-opacity-75 z-50">
+          <div className="p-4 absolute left-0 top-20 h-full flex flex-col items-start space-y-4">
+            {routes.map((route, index) => (
+              <Link
+                key={index}
+                to={route.path}
+                onClick={toggleDrawer}
+                className={`text-white hover:text-gray-300`}
+              >
+                {route.name}
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
     </nav>
-    // <nav className="bg-gray-800 p-4 flex items-center justify-between">
-    //   <div className="flex items-center space-x-4">
-    //     <Link to="/" className="text-white hover:text-gray-300">Home</Link>
-    //   </div>
-    //   <div className="flex items-center space-x-4">
-    //     <Link to="/cart" className="text-white hover:text-gray-300"><FaShoppingCart /></Link>
-    //     <Link to="/profile" className="text-white hover:text-gray-300"><FaUser /></Link>
-    //   </div>
-    // </nav>
   );
 };
 
